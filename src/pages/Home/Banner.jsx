a0b90807da1b83339346d20bd3c6fa1266302dea
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-12-08 14:41:33 +0600

    axios setup and navbar implementation

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 const Banner = () => {
-  return <div>Banner</div>;
+  const imgLinks = [
+    "https://i.ibb.co.com/xSDq5NZY/1.jpg",
+    "https://i.ibb.co.com/7J4gwnGv/2.jpg",
+    "https://i.ibb.co.com/1Gdhs5H8/3.png",
+  ];
+  return (
+    <div>
+      <Carousel
+        autoPlay
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+      >
+        {imgLinks.map((img) => (
+          <div>
+            <img className="h-[650px] object-cover" src={img} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
